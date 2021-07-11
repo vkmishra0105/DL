05eb60cf70fe7e2b76e314fcaf801f5aa495f32d
Vinay Mishra <64050130+vkmishra0105@users.noreply.github.com>
2021-07-10 23:03:24 -0500

    Update models.py

--- homework/models.py
+++ homework/models.py
@@ -1,26 +1,29 @@
 import torch
 import torch.nn.functional as F
 
-
 class CNNClassifier(torch.nn.Module):
     class Block(torch.nn.Module):
-        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
-            super().__init__()
-            self.c1 = torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
-                                      stride=stride, bias=False)
-            self.c2 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
-            self.c3 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
-            self.b1 = torch.nn.BatchNorm2d(n_output)
-            self.b2 = torch.nn.BatchNorm2d(n_output)
-            self.b3 = torch.nn.BatchNorm2d(n_output)
-            self.skip = torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
-
+        def __init__(self, n_input, n_output, kernel_size=3, stride=1):
+            super().__init__() 
+            self.net=torch.nn.Sequential(
+            torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
+                                      stride=stride, bias=False), 
+ #           torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False),
+            torch.nn.BatchNorm2d(n_output),
+            torch.nn.ReLU(),
+            torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False),
+           
+ #           torch.nn.BatchNorm2d(n_output),
+ #           
+  #          torch.nn.BatchNorm2d(n_output),
+ #           torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
+            )
         def forward(self, x):
-            return F.relu(self.b3(self.c3(F.relu(self.b2(self.c2(F.relu(self.b1(self.c1(x)))))))) + self.skip(x))
+            return (self.net(x))
 
     def __init__(self, layers=[16, 32, 64, 128], n_output_channels=6, kernel_size=3):
         super().__init__()
-        self.input_mean = torch.Tensor([0.3521554, 0.30068502, 0.28527516])
+        self.input_mean = torch.Tensor([0.1, 0.2, 0.28527516])
         self.input_std = torch.Tensor([0.18182722, 0.18656468, 0.15938024])
 
         L = []
@@ -34,7 +37,6 @@ class CNNClassifier(torch.nn.Module):
     def forward(self, x):
         z = self.network((x - self.input_mean[None, :, None, None].to(x.device)) / self.input_std[None, :, None, None].to(x.device))
         return self.classifier(z.mean(dim=[2, 3]))
-
 
 class FCN(torch.nn.Module):
     class UpBlock(torch.nn.Module):
@@ -82,12 +84,10 @@ class FCN(torch.nn.Module):
                 z = torch.cat([z, up_activation[i]], dim=1)
         return self.classifier(z)
 
-
 model_factory = {
     'cnn': CNNClassifier,
     'fcn': FCN,
 }
-
 
 def save_model(model):
     from torch import save
@@ -97,10 +97,84 @@ def save_model(model):
             return save(model.state_dict(), path.join(path.dirname(path.abspath(__file__)), '%s.th' % n))
     raise ValueError("model type '%s' not supported!" % str(type(model)))
 
-
 def load_model(model):
     from torch import load
     from os import path
     r = model_factory[model]()
     r.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), '%s.th' % model), map_location='cpu'))
     return r
+
+
+train_cnn.py HW3_sol
+from .models import CNNClassifier, save_model
+from .utils import accuracy, load_data
+import torch
+import torch.utils.tensorboard as tb
+
+def train(args):
+    from os import path
+    model = CNNClassifier()
+    train_logger, valid_logger = None, None
+    if args.log_dir is not None:
+        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
+        valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'))
+
+    import torch
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    model = CNNClassifier().to(device)
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=.01)
+    loss = torch.nn.CrossEntropyLoss()
+
+    train_data = load_data('data/train', batch_size=16)
+    valid_data = load_data('data/valid', batch_size=16)
+
+    global_step = 0
+    for epoch in range(35):
+        model.train()
+        acc_vals = []
+        for img, label in train_data:
+            img, label = img.to(device), label.to(device)
+
+            logit = model(img)
+            loss_val = loss(logit, label)
+            acc_val = accuracy(logit, label)
+
+            if train_logger is not None:
+                train_logger.add_scalar('loss', loss_val, global_step)
+            acc_vals.append(acc_val.detach().cpu().numpy())
+
+            optimizer.zero_grad()
+            loss_val.backward()
+            optimizer.step()
+            global_step += 1
+        avg_acc = sum(acc_vals) / len(acc_vals)
+
+        if train_logger:
+            train_logger.add_scalar('accuracy', avg_acc, global_step)
+
+        model.eval()
+        acc_vals = []
+        for img, label in valid_data:
+            img, label = img.to(device), label.to(device)
+            acc_vals.append(accuracy(model(img), label).detach().cpu().numpy())
+        avg_vacc = sum(acc_vals) / len(acc_vals)
+
+        if valid_logger:
+            valid_logger.add_scalar('accuracy', avg_vacc, global_step)
+
+        save_model(model)
+    save_model(model)
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--log_dir')
+    # Put custom arguments here
+
+    args = parser.parse_args()
+    train(args)
