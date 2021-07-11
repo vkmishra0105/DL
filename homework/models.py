b4d453e331858855d39c0b7afdb28b3b9f331fc8
Vinay Mishra <64050130+vkmishra0105@users.noreply.github.com>
2021-07-11 18:32:50 -0500

    Update models.py

--- homework/models.py
+++ homework/models.py
@@ -2,47 +2,71 @@ import torch
 import torch.nn.functional as F
 import numpy as py
 
+
 class CNNClassifier(torch.nn.Module):
-    class Block(torch.nn.Module):
-        def __init__(self, n_input, n_output, kernel_size=3, stride=1):
-            super().__init__() 
-            self.net=torch.nn.Sequential(
-            torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
-                                      stride=stride, bias=False), 
- #           torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False),
-            torch.nn.BatchNorm2d(n_output),
-            torch.nn.ReLU(),
-            torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False),
-           
- #           torch.nn.BatchNorm2d(n_output),
- #           
-  #          torch.nn.BatchNorm2d(n_output),
- #           torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
-            )
-        def forward(self, x):
-            return (self.net(x))
-
-    def __init__(self, layers=[16, 32, 64, 128], n_output_channels=6, kernel_size=3):
+    def __init__(self):
         super().__init__()
-        self.input_mean = torch.Tensor([0.1, 0.2, 0.4])
-        self.input_std = torch.Tensor([0.15, 0.16, 0.17])
+        self.net = torch.nn.Sequential(
+          torch.nn.Conv2d(3, 32, kernel_size=7, padding = 2),
+          torch.nn.BatchNorm2d(32),
+          torch.nn.ReLU(),
+          torch.nn.MaxPool2d(3,2,1),
+          torch.nn.Conv2d(32, 64, kernel_size=3, padding = 1),
+          torch.nn.BatchNorm2d(64),
+          torch.nn.ReLU(),
+          torch.nn.MaxPool2d(3,2,1),
+          torch.nn.Conv2d(64, 128, kernel_size=3, padding = 1),
+          torch.nn.BatchNorm2d(128),
+          torch.nn.ReLU(),
+          torch.nn.MaxPool2d(3,2,1),
+          torch.nn.Conv2d(128, 256, kernel_size=3, padding = 1),
+          torch.nn.BatchNorm2d(256),
+          torch.nn.ReLU(),
+          torch.nn.MaxPool2d(3,2,1),
+          torch.nn.Conv2d(256, 512, kernel_size=3, padding = 1),
+          torch.nn.BatchNorm2d(512),
+          torch.nn.ReLU(),
+          torch.nn.MaxPool2d(1, 1, 0)
+        )
+        self.netlinear=torch.nn.Sequential(
+          torch.nn.Linear(8192, 120),
+          torch.nn.Dropout(0.05),
+          torch.nn.Linear(120, 6)
+          
+        )
+        self.downsample = None
+        #if stride!=1
+        #  self.downsample = torch.nn.Sequential(
+        #    torch.nn.Conv2d()
+        #  )
 
-        L = []
-        c = 3
-        for l in layers:
-            L.append(self.Block(c, l, kernel_size, 2))
-            c = l
-        self.network = torch.nn.Sequential(*L)
-        self.classifier = torch.nn.Linear(c, n_output_channels)
+        #come back to this lol
+        #raise NotImplementedError('CNNClassifier.__init__')
 
     def forward(self, x):
-        z = self.network((x - self.input_mean[None, :, None, None].to(x.device)) / self.input_std[None, :, None, None].to(x.device))
-        return self.classifier(z.mean(dim=[2, 3]))
+        """
+        Your code here
+        @x: torch.Tensor((B,3,64,64))
+        @return: torch.Tensor((B,6))
+        """
+        identity = x
+        x = self.net(x)
+        x = x.view(x.size(0), -1)
+        x = self.netlinear(x)
+        return x
+
 
 class FCN(torch.nn.Module):
     def __init__(self):
         super().__init__()
-        
+        """
+        Your code here.
+        Hint: The FCN can be a bit smaller the the CNNClassifier since you need to run it at a higher resolution
+        Hint: Use up-convolutions
+        Hint: Use skip connections
+        Hint: Use residual connections
+        Hint: Always pad by kernel_size / 2, use an odd kernel_size
+        """
         self.net1 = torch.nn.Sequential(
           torch.nn.Conv2d(3, 32, kernel_size=3, padding = 2),
           torch.nn.BatchNorm2d(32),
@@ -66,17 +90,6 @@ class FCN(torch.nn.Module):
           torch.nn.ReLU(),
           torch.nn.MaxPool2d(3,2,1),
         )
-        self.net5 = torch.nn.Sequential(
-          torch.nn.Conv2d(256, 512, kernel_size=3, padding = 1),
-          torch.nn.BatchNorm2d(512),
-          torch.nn.ReLU(),
-          torch.nn.MaxPool2d(1, 1, 0)
-        )
-        self.fcn1 = torch.nn.Sequential(
-          torch.nn.ConvTranspose2d(512, 256, kernel_size=3, stride=2, padding=1, dilation=1, output_padding = 1),
-          torch.nn.BatchNorm2d(256),
-          torch.nn.ReLU()
-          )
         self.fcn2 = torch.nn.Sequential(
           torch.nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, dilation=1, output_padding = 1),
           torch.nn.BatchNorm2d(128),
@@ -115,10 +128,10 @@ class FCN(torch.nn.Module):
         x2 = self.net2(x1)
         x3 = self.net3(x2)
         x4 = self.net4(x3)
-        x5 = self.net5(x4)
-        x6 = self.fcn1(x5)
-        x6 = x6[:x.size(0), :, :x4.size(2), :x4.size(3)]
-        x7 = self.fcn2(torch.cat([x6, x4],0))
+        #x5 = self.net5(x4)
+        x7 = self.fcn2(x4)
+        #x6 = x6[:x.size(0), :, :x4.size(2), :x4.size(3)]
+        #x7 = self.fcn2(torch.cat([x6, x4],0))
         x7 = x7[:x.size(0), :, :x3.size(2), :x3.size(3)]
         x8 = self.fcn3(torch.cat([x7, x3],0))
         x8 = x8[:x.size(0), :, :x2.size(2), :x2.size(3)]
@@ -129,7 +142,8 @@ class FCN(torch.nn.Module):
        # x = self.fcn(x)
         xout = x10[:x.size(0), :, :size[2], :size[3]]
         return xout
-        raise NotImplementedError('FCN.forward')
+#        raise NotImplementedError('FCN.forward')
+
 
 model_factory = {
     'cnn': CNNClassifier,
@@ -152,3 +166,4 @@ def load_model(model):
     r = model_factory[model]()
     r.load_state_dict(load(path.join(path.dirname(path.abspath(__file__)), '%s.th' % model), map_location='cpu'))
     return r
+
