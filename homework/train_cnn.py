8ad62b9e7c1c945cc7e696e3332155df350bcf19
Vinay Mishra <64050130+vkmishra0105@users.noreply.github.com>
2021-07-10 23:06:26 -0500

    Update train_cnn.py

--- homework/train_cnn.py
+++ homework/train_cnn.py
@@ -1,93 +1,64 @@
 from .models import CNNClassifier, save_model
-from .utils import ConfusionMatrix, load_data, LABEL_NAMES
+from .utils import accuracy, load_data
 import torch
-import torchvision
 import torch.utils.tensorboard as tb
-
 
 def train(args):
     from os import path
     model = CNNClassifier()
     train_logger, valid_logger = None, None
     if args.log_dir is not None:
-        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'), flush_secs=1)
-        valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'), flush_secs=1)
+        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
+        valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'))
 
-    """
-    Your code here, modify your HW1 / HW2 code
-    
-    """
     import torch
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     model = CNNClassifier().to(device)
-    if args.continue_training:
-        model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'cnn.th')))
 
-    optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=0.9, weight_decay=1e-5)
+    optimizer = torch.optim.SGD(model.parameters(), lr=.01)
     loss = torch.nn.CrossEntropyLoss()
 
-    import inspect
-    transform = eval(args.transform,
-                     {k: v for k, v in inspect.getmembers(torchvision.transforms) if inspect.isclass(v)})
-    train_data = load_data('data/train', transform=transform, num_workers=4)
-    valid_data = load_data('data/valid', num_workers=4)
+    train_data = load_data('data/train', batch_size=16)
+    valid_data = load_data('data/valid', batch_size=16)
 
     global_step = 0
-    for epoch in range(args.num_epoch):
+    for epoch in range(35):
         model.train()
-        confusion = ConfusionMatrix(len(LABEL_NAMES))
+        acc_vals = []
         for img, label in train_data:
-            if train_logger is not None:
-                train_logger.add_images('augmented_image', img[:4])
             img, label = img.to(device), label.to(device)
 
             logit = model(img)
             loss_val = loss(logit, label)
-            confusion.add(logit.argmax(1), label)
+            acc_val = accuracy(logit, label)
 
             if train_logger is not None:
                 train_logger.add_scalar('loss', loss_val, global_step)
+            acc_vals.append(acc_val.detach().cpu().numpy())
 
             optimizer.zero_grad()
             loss_val.backward()
             optimizer.step()
             global_step += 1
+        avg_acc = sum(acc_vals) / len(acc_vals)
 
         if train_logger:
-            train_logger.add_scalar('accuracy', confusion.global_accuracy, global_step)
-            import matplotlib.pyplot as plt
-            f, ax = plt.subplots()
-            ax.imshow(confusion.per_class, interpolation='nearest', cmap=plt.cm.Blues)
-            for i in range(confusion.per_class.size(0)):
-                for j in range(confusion.per_class.size(1)):
-                    ax.text(j, i, format(confusion.per_class[i, j], '.2f'),
-                            ha="center", va="center", color="black")
-            train_logger.add_figure('confusion', f, global_step)
+            train_logger.add_scalar('accuracy', avg_acc, global_step)
 
         model.eval()
-        val_confusion = ConfusionMatrix(len(LABEL_NAMES))
+        acc_vals = []
         for img, label in valid_data:
             img, label = img.to(device), label.to(device)
-            val_confusion.add(model(img).argmax(1), label)
+            acc_vals.append(accuracy(model(img), label).detach().cpu().numpy())
+        avg_vacc = sum(acc_vals) / len(acc_vals)
 
         if valid_logger:
-            valid_logger.add_scalar('accuracy', val_confusion.global_accuracy, global_step)
-            import matplotlib.pyplot as plt
-            f, ax = plt.subplots()
-            ax.imshow(val_confusion.per_class, interpolation='nearest', cmap=plt.cm.Blues)
-            for i in range(val_confusion.per_class.size(0)):
-                for j in range(val_confusion.per_class.size(1)):
-                    ax.text(j, i, format(val_confusion.per_class[i, j], '.2f'),
-                            ha="center", va="center", color="black")
-            valid_logger.add_figure('confusion', f, global_step)
+            valid_logger.add_scalar('accuracy', avg_vacc, global_step)
 
-        if valid_logger is None or train_logger is None:
-            print('epoch %-3d \t acc = %0.3f \t val acc = %0.3f' % (epoch, confusion.global_accuracy,
-                                                                    val_confusion.global_accuracy))
         save_model(model)
-
+    save_model(model)
 
 if __name__ == '__main__':
     import argparse
@@ -96,13 +67,6 @@ if __name__ == '__main__':
 
     parser.add_argument('--log_dir')
     # Put custom arguments here
-    parser.add_argument('-n', '--num_epoch', type=int, default=29)
-    parser.add_argument('-lr', '--learning_rate', type=float, default=1e-2)
-    parser.add_argument('-c', '--continue_training', action='store_true')
-    parser.add_argument('-t', '--transform',
-                        default='Compose([ColorJitter(0.9, 0.9, 0.9, 0.1), RandomHorizontalFlip(), ToTensor()])')
 
     args = parser.parse_args()
     train(args)
-
-
