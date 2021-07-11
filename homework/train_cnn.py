fdc6354da025801bc4b9c0325720450b9e9c5664
Vinay Mishra <64050130+vkmishra0105@users.noreply.github.com>
2021-07-11 11:52:38 -0500

    Update train_cnn.py

--- homework/train_cnn.py
+++ homework/train_cnn.py
@@ -1,3 +1,4 @@
+
 from .models import CNNClassifier, save_model
 from .utils import accuracy, load_data
 import torch
@@ -11,20 +12,28 @@ def train(args):
         train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
         valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'))
 
+    """
+    Your code here, modify your HW1 code
+    
+    """
+    weight_decay=args.weight_decay
+    batch_size=args.batch_size
     import torch
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
     model = CNNClassifier().to(device)
+    if args.continue_training:
+        model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'cnn.th')))
 
-    optimizer = torch.optim.SGD(model.parameters(), lr=.01)
+    optimizer = torch.optim.SGD(model.parameters(), lr=args.learning_rate, momentum=0.9, weight_decay = weight_decay)
     loss = torch.nn.CrossEntropyLoss()
 
-    train_data = load_data('data/train', batch_size=16)
-    valid_data = load_data('data/valid', batch_size=16)
+    train_data = load_data('data/valid', batch_size=batch_size)
+    valid_data = load_data('data/train', batch_size=batch_size)
 
     global_step = 0
-    for epoch in range(35):
+    for epoch in range(args.num_epoch):
         model.train()
         acc_vals = []
         for img, label in train_data:
@@ -57,8 +66,11 @@ def train(args):
         if valid_logger:
             valid_logger.add_scalar('accuracy', avg_vacc, global_step)
 
+        if valid_logger is None or train_logger is None:
+            print('epoch %-3d \t acc = %0.3f \t val acc = %0.3f' % (epoch, avg_acc, avg_vacc))
         save_model(model)
     save_model(model)
+
 
 if __name__ == '__main__':
     import argparse
@@ -66,7 +78,12 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--log_dir')
-    # custom arguments here
+    # Put custom arguments here
+    parser.add_argument('-n', '--num_epoch', type=int, default=30)
+    parser.add_argument('-lr', '--learning_rate', type=float, default=7e-3)
+    parser.add_argument('-c', '--continue_training', action='store_true')
+    parser.add_argument('-b', '--batch_size', type=int, default=128)
+    parser.add_argument('-wd', '--weight_decay', type=float, default=1e-4)
 
     args = parser.parse_args()
     train(args)
